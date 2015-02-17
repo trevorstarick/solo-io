b7592a5d3f05edae9a3008b49c254f6be7f39d9d
Trevor Starick <trevor.starick@gmail.com>
2015-02-17 02:21:22 -0500

    Popped old version

--- app/js/main.old.js
+++ app/js/main.old.js
@@ -1,54 +1,49 @@
+var i = 0;
+var delay = 0;
+var rootTime = 0;
+
+var buffer = [];
+
+var playing = false;
+
+var AudioContext = window.AudioContext || window.webkitAudioContext;
+var ctx = new AudioContext();
+
 var client = new BinaryClient('ws://localhost:9000');
 
-var ctx;
-var source;
-var block = [];
-
-window.addEventListener('load', init, false);
-
-function init() {
-  try {
-    // Fix up for prefixing
-    window.AudioContext = window.AudioContext || window.webkitAudioContext;
-    ctx = new AudioContext();
-  } catch (e) {
-    alert('Web Audio API is not supported in this browser');
-  }
-}
-
-function stop() {
-  clearInterval(loop);
-}
-
 client.on('stream', function(stream, meta) {
-  var worker = new Worker("js/worker.js");
-  var workerPrime = new Worker("js/worker.js");
 
   stream.on('data', function(data) {
-    block.push(data);
+    buffer.push(data);
+
+    // ctx.decodeAudioData(data, function(data) {
+    //   buffer.push(data);
+    // });
   });
 
-  stream.on('end', function() {
-    var delay = 0;
-    var i = 0;
-    window.loop = setInterval(function() {
-      console.log(i);
-
-      if(i > block.length) {
-        stop();
-      }
-
-      delay = 5000;
-
-      ctx.decodeAudioData(block[i], function(data) {
-        source = ctx.createBufferSource();
-        source.connect(ctx.destination);
-        source.buffer = data;
-        source.start();
-      });
-
-      i+=1;
-    }, delay);
-
-  })
+  stream.on('end', main);
 });
+
+function main() {
+  var channels = 2; // 1 - mono, 2 - setreo, 3 < multi channel
+  var frequency = 44100;  // 44100 - standard
+  var length = 10 * 60 * frequency; //min * sec * freq
+
+  console.log(buffer);
+
+  async.eachSeries(buffer, function(v, cb) {
+    ctx.decodeAudioData(v, function(data) {
+
+      var source = ctx.createBufferSource();
+      source.buffer = data;
+      source.connect(ctx.destination);
+      source.start(0);
+
+      setTimeout(function() {
+        cb();
+      }, v.duration * 1000);
+
+    })
+  });
+
+}

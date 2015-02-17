744d807459f039e6c36bd9add98307fd82bbd42c
Trevor Starick <trevor.starick@gmail.com>
2015-02-17 02:21:02 -0500

    New server

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
 var fs = require('fs');
+var io = require('socket.io')(8080);
 
 var express = require('express');
-var BinaryServer = require('binaryjs').BinaryServer;
-
 var app = express();
-var server = BinaryServer({port: 9000});
 
 app.use('/vendor', express.static(__dirname + '/vendor'));
 app.use(express.static(__dirname + '/app'));
 
-server.on('connection', function(client) {
-  // fs.readdir(__dirname + '/data/', function(err, files) {
-  //   files.forEach(function(value, index, array) {
-  //       var file = fs.createReadStream(__dirname + '/data/' + value);
-  //       if(index < limit) {
-  //         client.send(file);
-  //       }
-  //   });
-  // });
-  var file = fs.createReadStream(__dirname + '/example.mp3');
-  var stream = client.createStream();
-  file.pipe(stream);
+app.get('/file', function(req, res) {
+  var file = fs.createReadStream(__dirname + '/audio/02_Ghosts_I.ogg');
+  file.on('open', function() {
+    file.pipe(res);
+  });
 })
 
+io.on('connection', function(socket) {
+  var file = fs.createReadStream(__dirname + '/audio/02_Ghosts_I.mp3');
+  file.on('data', function(data){
+    socket.emit('packet', data);
+  });
+});
 
 app.listen(80);
